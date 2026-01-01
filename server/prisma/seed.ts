80a48aee7aaed3c61dd27a312622bfaa7cb8946a
Abhijith Selvam <abselvam44@gmail.com>
2026-01-01 18:49:35 +0530

    updated seed.ts

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,28 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  console.error("DATABASE_URL not found in process.env!");
-  process.exit(1); // Stop the script if DATABASE_URL is missing
-}
-console.log("DATABASE_URL detected:", databaseUrl);
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 import path from "path";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-// const prisma = new PrismaClient();
-
-// Create a Postgres pool
-const pool = new Pool({
-  connectionString: databaseUrl,
-});
-
-// Export adapter for PrismaClient
-export const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 async function deleteAllData(orderedFileNames: string[]) {
   const modelNames = orderedFileNames.map((fileName) => {
@@ -30,19 +9,7 @@ async function deleteAllData(orderedFileNames: string[]) {
     return modelName.charAt(0).toUpperCase() + modelName.slice(1);
   });
 
-  // for (const modelName of modelNames) {
-  //   const model: any = prisma[modelName as keyof typeof prisma];
-  //   if (model) {
-  //     await model.deleteMany({});
-  //     console.log(`Cleared data from ${modelName}`);
-  //   } else {
-  //     console.error(
-  //       `Model ${modelName} not found. Please ensure the model name is correctly specified.`
-  //     );
-  //   }
-  // }
-  for (const modelName of modelNames.reverse()) {
-    // REVERSE order
+  for (const modelName of modelNames) {
     const model: any = prisma[modelName as keyof typeof prisma];
     if (model) {
       await model.deleteMany({});
@@ -60,14 +27,14 @@ async function main() {
 
   const orderedFileNames = [
     "products.json",
+    "expenseSummary.json",
+    "sales.json",
+    "salesSummary.json",
+    "purchases.json",
+    "purchaseSummary.json",
     "users.json",
     "expenses.json",
-    "sales.json",
-    "purchases.json",
-    "expenseSummary.json", // <- seed parent first
-    "expenseByCategory.json", // <- then seed child
-    "salesSummary.json",
-    "purchaseSummary.json",
+    "expenseByCategory.json",
   ];
 
   await deleteAllData(orderedFileNames);
